16d411315c19de854e7f7925685aeae21378e960
Sme <esahig15789@gmail.com>
2020-06-13 23:48:15 -0400

    hehe

--- Wizardry/SmeHax/FixedDamageWeapons/FixedDamageWeapons.s
+++ Wizardry/SmeHax/FixedDamageWeapons/FixedDamageWeapons.s
@@ -41,6 +41,27 @@ mov r1,r4
 add r1,#0x5A
 strh r0,[r1]
 
+
+@but also, is the weapon effective vs. the target?
+.equ IsWeaponEffectiveAgainst,0x8016BEC @r0 = item halfword, r1 = target unit ptr
+ldr r0,=IsWeaponEffectiveAgainst
+mov r14,r0
+mov r0,r4
+add r0,#0x4A
+ldrh r0,[r0]
+mov r1,r5
+.short 0xF800
+cmp r0,#0
+beq GoBack
+
+@it is, so let's triple the damage
+mov r1,r4
+add r1,#0x5A
+ldrh r0,[r1]
+lsl r2,r0,#1 @*2
+add r0,r2 @*3
+strh r0,[r1]
+
 @put this at the end of the pre battle calc loop
 
 GoBack:
@@ -50,4 +71,3 @@ bx r0
 
 .ltorg
 .align
-
